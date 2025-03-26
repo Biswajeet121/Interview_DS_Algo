eca109e3f76af1fd54a31c0d7b9a693267b93484
Biswajeet Samal <162594789+Biswajeet121@users.noreply.github.com>
2025-03-26 22:12:50 +0530

    Create Word Break.cpp

---
+++ DP - 1D/Word Break.cpp
@@ -0,0 +1,28 @@
+
+  Problem Link : https://www.geeksforgeeks.org/problems/word-break1352/1
+
+class Solution {
+  public:
+    bool wordBreak(string &s, vector<string> &dic) {
+        // code here
+        int n=s.size(),m=dic.size();
+        vector<int>dp(n+1,0);
+        dp[n]=1;
+        for(int i=n-1;i>=0;i--){
+            for(int j=m-1;j>=0;j--){
+                int k=0;
+                for(int l=i;l<n&&k<dic[j].size();l++){
+                    if(s[l]==dic[j][k]){
+                        k++;
+                    }else{
+                        break;
+                    }
+                }
+                if(k==dic[j].size()){
+                    dp[i]|=dp[i+k];
+                }
+            }
+        }
+        return dp[0];
+    }
+};
