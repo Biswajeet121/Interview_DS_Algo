2c78d88b1d34473d5bb8c2ed15d49eecc449afc8
Biswajeet Samal <162594789+Biswajeet121@users.noreply.github.com>
2025-04-16 21:37:34 +0530

    Create  Floyd Warshall.cpp

---
+++ Graph/Floyd Warshall.cpp
@@ -0,0 +1,19 @@
+
+  Problem Link : https://www.geeksforgeeks.org/batch/gfg-160-problems/track/graph-gfg-160/problem/implementing-floyd-warshall2042
+
+class Solution {
+  public:
+    void floydWarshall(vector<vector<int>> &dist) {
+        // Code here
+        int n = dist.size();
+        for(int via=0; via<n; via++){
+            for(int i=0; i<n; i++){
+                for(int j=0; j<n; j++){
+                    if(dist[i][via]!=100000000 && dist[via][j]!=100000000 && dist[i][j] > dist[i][via] + dist[via][j]){
+                        dist[i][j] = dist[i][via] + dist[via][j];
+                    }
+                }
+            }
+        }
+    }
+};
